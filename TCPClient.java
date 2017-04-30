6ccc941cd2a19514f7cc247bb819c1c07dae7940
unknown <>
2017-04-30 15:29:52 +0800

    1.6

--- TCPClient.java
+++ TCPClient.java
@@ -4,15 +4,21 @@ class TCPClient{
     public static void main(String argv[]) throws Exception{
         String sentence;
         String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        //InetAddress remoteAddr1 = InetAddress.getByName("118.89.162.148");
-        Socket clientSocket = new Socket(InetAddress.getByName("118.89.162.148"),6789);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        sentence = inFromUser.readLine();
-        outToServer.writeBytes(sentence + '\n');
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("From server: " + modifiedSentence);
+        while(true){
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            //创建socket对象
+            Socket clientSocket = new Socket(InetAddress.getByName("118.89.162.148"),6789);
+            //为进程提供到套接字的输出
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            //接受套接字的输入
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            sentence = inFromUser.readLine();
+            //发送至TCP管道
+            outToServer.writeBytes(sentence + '\n');
+            //读取结果
+            modifiedSentence = inFromServer.readLine();
+            System.out.println("From server: " + modifiedSentence);
+        }
         clientSocket.close();
     }
 }
--- TCPServer.java
+++ TCPServer.java
@@ -5,9 +5,12 @@ class TCPServer{
     public static void main(String argv[]) throws Exception{
         String clientSentence;
         String capitalizedSentence;
+        //监听6789端口
         ServerSocket welcomeSocket = new ServerSocket(6789);
         while(true){
+            //建立管道
             Socket connectionSocket = welcomeSocket.accept();
+            System.out.println(connectionSocket);
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream outToclient = new DataOutputStream(connectionSocket.getOutputStream());
             clientSentence = inFromClient.readLine();

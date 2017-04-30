da56f088e9ae9bf332f56149a441bbc822319f86
unknown <>
2017-04-30 14:46:54 +0800

    1.1

--- TCPServer.java
+++ TCPServer.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 
 class TCPServer{
-    public static void main(Strin argv[]) throws Exception{
+    public static void main(String argv[]) throws Exception{
         String clientSentence;
         String capitalizedSentence;
         ServerSocket welcomeSocket = new ServerSocket(6789);

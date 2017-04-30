7af631e7145cf362239b1df65cd84e0505411628
unknown <>
2017-04-30 15:06:21 +0800

    1.5

--- TCPClient.java
+++ TCPClient.java
@@ -5,10 +5,11 @@ class TCPClient{
         String sentence;
         String modifiedSentence;
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("118.89.162.148",6789);
+        //InetAddress remoteAddr1 = InetAddress.getByName("118.89.162.148");
+        Socket clientSocket = new Socket(InetAddress.getByName("118.89.162.148"),6789);
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        sentence = inFromServer.readLine();
+        sentence = inFromUser.readLine();
         outToServer.writeBytes(sentence + '\n');
         modifiedSentence = inFromServer.readLine();
         System.out.println("From server: " + modifiedSentence);

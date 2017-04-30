f92ae05a7495c8c29e6cb37375bebecdd052b71a
unknown <>
2017-04-30 14:51:07 +0800

    1.1.2

--- TCPClient.java
+++ TCPClient.java
@@ -4,7 +4,7 @@ class TCPClient{
     public static void main(String argv[]) throws Exception{
         String sentence;
         String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(system.in));
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         Socket clientSocket = new Socket("118.89.162.148",6789);
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
